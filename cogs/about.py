56c55349e2711aaf4f2ec664853e26c56b6346ed
aic-6301 <rx435423@gmail.com>
2025-06-02 23:02:05 +0900

    about updated

--- cogs/about.py
+++ cogs/about.py
@@ -11,7 +11,7 @@ class about(commands.Cog):
     async def about(self, interaction: discord.Interaction):
         embed = discord.Embed(
             title="再生してるやつMisskeyに送るやつ",
-            description="""このボットは、Spotifyで再生中の曲をMisskeyに送信します。
+            description="""このボットは、Spotifyで再生中の曲をMisskeyに送信します。(今のところSpotifyのみ対応。そのうちMusic Presenceにも対応するかも。)
 このBotはMisskeyとDiscordの連携を行うため、Tokenを保存していますが、作成者は、Tokenを使用したり、公開したりすることは必ず行いません。
 ユーザーは、自分のTokenを削除したくなった場合、/unregisterコマンドを使用して、Botから情報を削除することができるものとします。""",
             color=discord.Color.blue()
